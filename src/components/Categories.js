3842688556d5e1b6604d2c18c9fd2d15e0ca4508
Medo El-Bahrawy <mohamedelbahrawy512@gmail.com>
2023-10-31 00:15:27 +0200

    add favicon

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -3,24 +3,25 @@ import ing from "../img/engineer-worker-svgrepo-com.png";
 import elv from "../img/elevator-svgrepo-com.png";
 import buld from "../img/buildings-building-svgrepo-com.png";
 import "./Categories.css";
+import { Link } from "react-router-dom";
 export default function Categories() {
   return (
     <div className="categories">
       <div className="container">
         <h2 className="text-center">بوابتك لعالم المصاعد الذكية في مصر</h2>
         <div className="row">
-          <div className="category col-md-4 col-sm-12">
+          <Link to="/technicians" className="category col-md-4 col-sm-12">
             <img src={ing} alt="" />
             <h3>الفنيين</h3>
-          </div>
-          <div className="category col-md-4 col-sm-12">
+          </Link>
+          <Link to="/" className="category col-md-4 col-sm-12">
             <img src={elv} alt="" />
             <h3>عروض المصاعد</h3>
-          </div>
-          <div className="category col-md-4 col-sm-12">
+          </Link>
+          <Link to="/Companies" className="category col-md-4 col-sm-12">
             <img src={buld} alt="" />
             <h3>الشركات</h3>
-          </div>
+          </Link>
         </div>
       </div>
     </div>
